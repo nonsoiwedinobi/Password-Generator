de5bcc3ddcc4c073f4a38a2b1bfd7518a9908a74
Nonso <nonsoiwedinobi@yahoo.com>
2023-10-29 23:25:11 +0000

    Added html

--- starter/script.js
+++ starter/script.js
@@ -90,17 +90,65 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var passwordLength = parseInt(prompt("Enter the length of your password (between 8 and 128 characters):"));
 
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must be a number between 8 and 128.");
+    return null; // Exit the function
+  }
+
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("You must select at least one character type.");
+    return null; // Exit the function
+  }
+
+  return {
+    length: passwordLength,
+    includeLowercase: includeLowercase,
+    includeUppercase: includeUppercase,
+    includeNumeric: includeNumeric,
+    includeSpecial: includeSpecial
+  };
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  var options = getPasswordOptions();
+  if (!options) return null; // User canceled or entered invalid input
 
+  var possibleCharacters = [];
+  var result = [];
+
+  if (options.includeLowercase) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+  }
+  if (options.includeUppercase) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+  }
+  if (options.includeNumeric) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+  }
+  if (options.includeSpecial) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+  }
+
+  for (var i = 0; i < options.length; i++) {
+    var randomCharacter = getRandom(possibleCharacters);
+    result.push(randomCharacter);
+  }
+
+  return result.join('');
 }
 
 // Get references to the #generate element
@@ -111,7 +159,9 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+  if (password) {
+    passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
